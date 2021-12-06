bb9543cf3d96fa3b668f41170eff12d7fa1ea510
Nathakorn Throngtrairat <nathakorn.throngtrairat@agoda.com>
2021-12-07 01:32:05 +0700

    add component to entity

--- ecs/src/entity.rs
+++ ecs/src/entity.rs
@@ -1,3 +1,5 @@
+use std::{any::{Any, TypeId}, collections::HashMap};
+
 pub type EntityId = u64;
 
 #[derive(Debug)]
@@ -5,14 +7,16 @@ pub struct Entity {
     pub id: EntityId,
     pub tag: String,
     alive: bool,
+    components: HashMap<TypeId, Box<dyn Any>>
 }
 
 impl Entity {
-    pub(crate) const fn new(id: EntityId, tag: String) -> Self {
+    pub(crate) fn new(id: EntityId, tag: String) -> Self {
         Self {
             id,
             alive: true,
             tag,
+            components: HashMap::new()
         }
     }
 
@@ -22,5 +26,53 @@ impl Entity {
 
     pub fn is_alive(&self) -> bool {
         self.alive
+    }
+
+    pub fn add_component<T: Any>(&mut self, component: T) -> &mut Self {
+        self.components.insert(TypeId::of::<T>(), Box::new(component));
+        self
+    }
+
+    pub fn get_component<'a, T: Any>(&'a mut self) -> Option<&'a mut T> {
+        self.components
+            .get_mut(&TypeId::of::<T>())
+            .and_then(|v| v.downcast_mut::<T>())
+    }
+
+    pub fn has_component<T: Any>(&self) -> bool {
+        self.components.contains_key(&TypeId::of::<T>())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Entity;
+
+    struct MyComponent;
+
+    struct OtherComponent;
+
+    #[test]
+    fn test_get_component_not_exist() {
+        let mut entity = Entity::new(1, "".to_string());
+        let comp = MyComponent;
+
+        entity.add_component(comp);
+
+        let res = entity.get_component::<OtherComponent>();
+        assert!(res.is_none());
+        assert!(!entity.has_component::<OtherComponent>())
+    }
+
+    #[test]
+    fn test_get_component() {
+        let mut entity = Entity::new(1, "".to_string());
+        let comp = MyComponent;
+
+        entity.add_component(comp);
+
+        let res = entity.get_component::<MyComponent>();
+        assert!(res.is_some());
+        assert!(entity.has_component::<MyComponent>())
     }
 }
