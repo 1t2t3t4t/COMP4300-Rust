727cec79775b61598a75220e9c40f421cf91eca3
Nathakorn Throngtrairat <nathakorn.throngtrairat@agoda.com>
2021-12-12 01:22:16 +0700

    Revert "cheat add profiler"
    This reverts commit c29563f7841c30d913f95e3887aba8dc6aee2b93.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::mem::MaybeUninit;
 use crate::space_shooter::SpaceGame;
 use ggez::{conf::WindowMode, ContextBuilder, GameResult};
-use pprof::ProfilerGuard;
 
 mod common;
 mod game;
@@ -12,22 +10,7 @@ mod ui;
 const WINDOWS_WIDTH: f32 = 1280f32;
 const WINDOWS_HEIGHT: f32 = 720f32;
 
-static mut PROFILER: MaybeUninit<ProfilerGuard> = MaybeUninit::uninit();
-const ONCE: std::sync::Once = std::sync::Once::new();
-
-pub fn profiler() -> &ProfilerGuard {
-    ONCE.call_once(|| {
-        unsafe {
-            PROFILER.write(ProfilerGuard::new(100).unwrap().blocklist(&["libc", "libgcc", "pthread"]));
-        }
-    });
-    unsafe {
-        PROFILER.assume_init_ref()
-    }
-}
-
 fn main() -> GameResult<()> {
-    profiler();
     let (ctx, event_loop) = ContextBuilder::new("Comp4300", "Boss")
         .window_mode(WindowMode::default().dimensions(WINDOWS_WIDTH, WINDOWS_HEIGHT))
         .build()?;
