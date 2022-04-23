d85886c6e3abf298115ec4ef7f48e3d7b5b05e5f
nthrongtrair <nathakorn.throngtrairat@agoda.com>
2022-04-24 03:02:23 +0700

    make multiple types queryable

--- ecs/src/entity.rs
+++ ecs/src/entity.rs
@@ -1,6 +1,6 @@
 use std::{
     any::{Any, TypeId},
-    collections::HashMap,
+    collections::HashMap, vec,
 };
 
 pub type EntityId = u64;
@@ -54,15 +54,72 @@ impl Entity {
     pub fn has_component<T: Any>(&self) -> bool {
         self.components.contains_key(&TypeId::of::<T>())
     }
+
+    pub fn has_components<T: TypesQueryable>(&self) -> bool {
+        let query_types = T::get_types();
+        for t in query_types {
+            if !self.components.contains_key(&t) {
+                return false;
+            }     
+        }
+        true
+    }
 }
+
+pub trait TypesQueryable {
+    fn get_types() -> Vec<TypeId>;
+}
+
+macro_rules! types_queryable {
+    ($($t:ident),+) => {
+        impl<$($t), +> TypesQueryable for ($($t), +) where $($t : Any),+ {
+            fn get_types() -> Vec<TypeId> {
+                vec![
+                    $(TypeId::of::<$t>()),+
+                ]
+            }
+        }
+    };
+}
+
+// Auto implement tuple query typeid getter
+types_queryable!(A, B);
+types_queryable!(A, B, C);
+types_queryable!(A, B, C, D);
+types_queryable!(A, B, C, D, E);
+types_queryable!(A, B, C, D, E, F);
+types_queryable!(A, B, C, D, E, F, G);
+types_queryable!(A, B, C, D, E, F, G, H);
+types_queryable!(A, B, C, D, E, F, G, H, I);
+types_queryable!(A, B, C, D, E, F, G, H, I, J);
+types_queryable!(A, B, C, D, E, F, G, H, I, J, K);
 
 #[cfg(test)]
 mod tests {
-    use super::Entity;
+    use std::any::TypeId;
+
+    use super::{Entity, TypesQueryable};
 
     struct MyComponent;
 
     struct OtherComponent;
+
+    fn get_types<T: TypesQueryable>() -> Vec<TypeId> {
+        T::get_types()
+    }
+
+    #[test]
+    fn test_query_types() {
+        let types = get_types::<(MyComponent, OtherComponent)>();
+
+        assert_eq!(types[0], TypeId::of::<MyComponent>());
+        assert_eq!(types[1], TypeId::of::<OtherComponent>());
+    }
+
+    #[test]
+    fn test() {
+        
+    }
 
     #[test]
     fn test_get_component_not_exist() {
@@ -86,5 +143,40 @@ mod tests {
         let res = entity.get_component_mut::<MyComponent>();
         assert!(res.is_some());
         assert!(entity.has_component::<MyComponent>())
+    }
+
+    #[test]
+    fn test_check_components() {
+        struct RandomComponent;
+        
+        let mut entity = Entity::new(1, "".to_string());
+        entity.add_component(MyComponent);
+        entity.add_component(OtherComponent);
+
+        assert!(entity.has_component::<MyComponent>());
+        assert!(entity.has_component::<OtherComponent>());
+        assert!(!entity.has_component::<RandomComponent>());
+    }
+
+    #[test]
+    fn test_check_multi_components() {
+        let mut entity = Entity::new(1, "".to_string());
+        entity.add_component(MyComponent);
+        entity.add_component(OtherComponent);
+
+        assert!(entity.has_components::<(MyComponent, OtherComponent)>());
+        assert!(entity.has_components::<(OtherComponent, MyComponent)>());
+    }
+
+    #[test]
+    fn test_check_multi_components_mismatch() {
+        struct RandomComponent;
+
+        let mut entity = Entity::new(1, "".to_string());
+        entity.add_component(MyComponent);
+        entity.add_component(RandomComponent);
+
+        assert!(!entity.has_components::<(MyComponent, OtherComponent)>());
+        assert!(!entity.has_components::<(OtherComponent, MyComponent)>());
     }
 }
